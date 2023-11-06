ccbc7f8dfd63ca74da190f514a58ef9ddd3271a8
Jenin82 <jenin8282@gmail.com>
2023-11-06 22:12:25 +0530

    feat(): PWA setup done

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        VitePWA({
+            registerType: "autoUpdate",
+            injectRegister: "auto",
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+            },
+            includeAssets: [
+                "favicon.ico",
+                "public/apple-touch-icon.png",
+                "mask-icon.svg",
+            ],
+            manifest: {
+                name: "EachPays",
+                short_name: "EachPays",
+                description: "An app track your group expenses",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "public/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "public/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "public/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "public/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+    ],
+});
