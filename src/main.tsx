13b5f3b01a3b58a080cfc62dd9f8c4307ca3d303
Jenin82 <jenin8282@gmail.com>
2023-11-07 06:27:40 +0530

    feat(): PWA setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
+import { registerSW } from "virtual:pwa-register";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+interface UpdatePromptProps {
+    onUpdate: () => void; // This specifies that onUpdate is a function that returns void
+}
+
+// Use the props in the component
+const UpdatePrompt: React.FC<UpdatePromptProps> = ({ onUpdate }) => {
+    return (
+        <div>
+            New version available!
+            <button onClick={onUpdate}>Update</button>
+        </div>
+    );
+};
+
+const Main = () => {
+    const [showUpdate, setShowUpdate] = useState(false);
+    const [updateSW, setUpdateSW] = useState<() => Promise<void>>(
+        () => Promise.resolve
+    );
+
+    useEffect(() => {
+        const updateSW = registerSW({
+            onNeedRefresh() {
+                setShowUpdate(true);
+            },
+            onOfflineReady() {
+                // Handle offline readiness, perhaps with a toast notification
+            },
+        });
+
+        // Store the update function
+        setUpdateSW(() => updateSW);
+    }, []);
+
+    const onUpdate = async () => {
+        await updateSW(); // Call without arguments
+        window.location.reload(); // Reload the page after the update
+    };
+
+    return (
+        <React.StrictMode>
+            {showUpdate && <UpdatePrompt onUpdate={onUpdate} />}
+            <App />
+        </React.StrictMode>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<Main />);
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,53 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
     plugins: [
         VitePWA({
-            registerType: "autoUpdate",
+            registerType: "prompt",
             injectRegister: "auto",
             workbox: {
                 globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                        handler: "CacheFirst",
+                        options: {
+                            cacheName: "google-fonts-cache",
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+                        handler: "CacheFirst",
+                        options: {
+                            cacheName: "gstatic-fonts-cache",
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                    {
+                        handler: "NetworkOnly",
+                        urlPattern: /\/api\/.*\/*.json/,
+                        method: "POST",
+                        options: {
+                            backgroundSync: {
+                                name: "myQueueName",
+                                options: {
+                                    maxRetentionTime: 24 * 60,
+                                },
+                            },
+                        },
+                    },
+                ],
             },
             includeAssets: [
                 "favicon.ico",
@@ -43,6 +86,7 @@ export default defineConfig({
                     },
                 ],
             },
+            strategies: "generateSW",
         }),
     ],
 });
