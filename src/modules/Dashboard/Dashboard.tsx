f679721366c9f17e15b956ca100cf3b107a02c9d
Jenin82 <jenin8282@gmail.com>
2023-11-08 06:38:09 +0530

    feat(): dashboard css done

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -5,9 +5,11 @@ import {
     getDataFromLocalStorage,
 } from "../../utils/common";
 import { useNavigate } from "react-router-dom";
+import styles from "./Dashboard.module.css";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 export const Dashboard = () => {
-	const navigate = useNavigate()
+    const navigate = useNavigate();
     const [data, setData] = useState<Data>({ data: [] });
     useEffect(() => {
         // Load data from local storage on component mount
@@ -15,23 +17,36 @@ export const Dashboard = () => {
         if (localData) {
             setData(localData);
         }
-		deleteGroupsWithEmptyId(data);
+        deleteGroupsWithEmptyId(data);
     }, []);
 
     return (
-        <div>
-            <div>Test</div>
-            <button
-                onClick={() =>
-                    navigate('/create')
-                }
-            >
-                click
-            </button>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1>EACHPAYS</h1>
+            </div>
+            <div className={styles.logo}>
+                <img src="/logo.png?url" alt="Logo" />
+            </div>
+            <div className={styles.heading}>
+                <h3>Track your group expenses and split the bill</h3>
+            </div>
+            <div className={styles.button}>
+                <button onClick={() => navigate("/create")}>
+                    Create
+                    <AiOutlinePlusCircle />
+                </button>
+            </div>
+            <div className={styles.groups}>
                 {data.data.map((group) => (
-                    <div key={group.id}>
-                        <h2 onClick={() => navigate(`/group/${group.id}`)}>{group.name}</h2>
+                    <div key={group.id} className={styles.group}>
+                        <h2 onClick={() => navigate(`/group/${group.id}`)}>
+                            {group.name}
+                        </h2>
+                        <div>
+							<span>Total: {group.total}</span>
+                            <span>Members: {group.membersCount}</span>
+                        </div>
                     </div>
                 ))}
             </div>
